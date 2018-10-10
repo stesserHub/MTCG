0536cb7f91987f0cfa09ed6b0c60e6c51b37e811
Christoffer Rahbek <christoffer_rahbek@msn.com>
2018-10-10 22:13:14 +0200

    Every name must have a unique name true if false, or false thus true

--- Assets/JS/Game.js
+++ Assets/JS/Game.js
@@ -4,12 +4,22 @@ console.log("It is alive");
 const players = [];
 
 function addPlayer() {
+    // Participants
+    console.log(players);
+    // Get value from input 
     let name = document.getElementById("PlayerName").value;
+    // Compare potential participant against player array
+    let uniqueName = players.includes(name);
+    console.log(uniqueName);
+
+    // Name of potential participant
     console.log(name + " was detected");
-    if (name !== "") {
+    if (name !== "" && uniqueName == false) {
         players.push(name);
         console.log(players);
-    }
+    } else {
+        alert("invalid player name");
+    };
     //players.push();
     //document.getElementById("PlayerList").appendChild(document.createElement('li').innerHTML = "name"); 
 };
