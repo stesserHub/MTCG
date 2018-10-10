7532bd0ad0ea597fe8d09465d5f0ca8e62d5ad92
Christoffer Rahbek <christoffer_rahbek@msn.com>
2018-10-10 21:59:28 +0200

    Added players to players array

--- Assets/JS/Game.js
+++ Assets/JS/Game.js
@@ -1,7 +1,18 @@
 // Modulus The Card Game
 console.log("It is alive");
 // Players
-const players = [""];
+const players = [];
+
+function addPlayer() {
+    let name = document.getElementById("PlayerName").value;
+    console.log(name + " was detected");
+    if (name !== "") {
+        players.push(name);
+        console.log(players);
+    }
+    //players.push();
+    //document.getElementById("PlayerList").appendChild(document.createElement('li').innerHTML = "name"); 
+};
 // End game function
 function gameOver() {
     var r = confirm("The game is over.\nPlayer won!\nOK to play again?");
