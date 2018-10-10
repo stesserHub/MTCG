93e6016c6ff41a2781208f70d4e7bc42782269c9
Christoffer Rahbek <christoffer_rahbek@msn.com>
2018-10-10 22:41:28 +0200

    Working on adding a li for each new participant

--- Assets/JS/Game.js
+++ Assets/JS/Game.js
@@ -1,7 +1,17 @@
 // Modulus The Card Game
+
+// Establish connectivity
 console.log("It is alive");
+
+// Global Variables
 // Players
 const players = [];
+
+let playerList = document.getElementById("PlayerList");
+let listedPlayer = document.createElement("li");
+
+console.log(playerList);
+console.log(listedPlayer);
 
 function addPlayer() {
     // Participants
@@ -14,15 +24,22 @@ function addPlayer() {
 
     // Name of potential participant
     console.log(name + " was detected");
+    // Addint valid participant
     if (name !== "" && uniqueName == false) {
         players.push(name);
         console.log(players);
+        // So I need one list item per participant
+        for (player in players) {
+            playerList.appendChild(listedPlayer).appendChild(document.createTextNode(player));
+        }
+        //listedPlayer.appendChild(document.createTextNode(name));
+        //playerList.appendChild(listedPlayer);
     } else {
         alert("invalid player name");
     };
-    //players.push();
-    //document.getElementById("PlayerList").appendChild(document.createElement('li').innerHTML = "name"); 
+    
 };
+
 // End game function
 function gameOver() {
     var r = confirm("The game is over.\nPlayer won!\nOK to play again?");
