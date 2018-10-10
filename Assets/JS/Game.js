1f47885a77bddde7cd7d51cb1b7952897f4afcab
Christoffer Rahbek <christoffer_rahbek@msn.com>
2018-10-10 19:15:54 +0200

    End game button and gameOver function to reload page

--- Assets/JS/Game.js
+++ Assets/JS/Game.js
@@ -1,2 +1,13 @@
 // Modulus The Card Game
 console.log("It is alive");
+// Players
+const players = [""];
+// End game function
+function gameOver() {
+    var r = confirm("The game is over.\nPlayer won!\nOK to play again?");
+    if (r == true) {
+        window.location.reload();
+    } else {
+        console.log("Stay on page");
+    }
+};
